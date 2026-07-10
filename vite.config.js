a104a3754f65038fd70770d57d61d0713803a1b1
Naeem <naeem@Mac.vodafone.ultrahub>
2026-07-10 10:21:18 +0100

    fix: mark maplibre-gl as vite external to fix CI build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+export default {
+  optimizeDeps: { exclude: ['maplibre-gl'] },
+  build: { commonjsOptions: { include: [/maplibre-gl/, /node_modules/] } },
+
   plugins: [react()],
   server: { host: true },
 });
